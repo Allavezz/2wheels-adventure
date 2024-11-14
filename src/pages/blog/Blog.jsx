2b05d7243738c5a1916cfc7d0e3a2408f6b0fc3d
Allavezz <ricardogeraldesalves@gmail.com>
2024-11-14 13:50:15 +0000

    change image paths

--- src/pages/blog/Blog.jsx
+++ src/pages/blog/Blog.jsx
@@ -2,8 +2,8 @@ import Hero from '../../components/Hero';
 import BlogPosts from './BlogPosts';
 import Contacts from '../../components/Contacts';
 import BlogIntro from './BlogIntro';
-import blogHero from '/public/assets/blog/blogHero.jpg';
-import blogContact from '/public/assets/blog/blogContact.jpg';
+import blogHero from '/2wheels-adventure/assets/blog/blogHero.jpg';
+import blogContact from '/2wheels-adventure/assets/blog/blogContact.jpg';
 
 const Blog = () => {
 	return (
--- src/pages/contact/Contact.jsx
+++ src/pages/contact/Contact.jsx
@@ -2,8 +2,8 @@ import Hero from '../../components/Hero';
 import Contacts from '../../components/Contacts';
 import ContactIntro from './ContactIntro';
 import CTA from '../../components/CTA';
-import contactHero from '/public/assets/contact/contactHero.jpg';
-import contactContact from '/public/assets/contact/contactContact.jpg';
+import contactHero from '/2wheels-adventure/assets/contact/contactHero.jpg';
+import contactContact from '/2wheels-adventure/assets/contact/contactContact.jpg';
 
 const Contact = () => {
 	return (
